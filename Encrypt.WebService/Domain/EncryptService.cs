6c00cd716b4182ad03aa85cbcbf2bc26a01a84d6
Vicente Estrada Dominguez <vicent-e@ra.mx>
2024-06-24 12:47:18 -0600

    first commit

---
+++ Encrypt.WebService/Domain/EncryptService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using System.Text;
+
+namespace Encrypt.WebService.Domain
+{
+    public class EncryptService
+    {
+        private readonly IDataProtector _dataProtector;
+        public EncryptService(IDataProtectionProvider dataProtectionProvider)
+        {
+            _dataProtector = dataProtectionProvider.CreateProtector("Card");
+        }
+
+        public byte[] EncryptData(string plainText)
+        {
+            var bytes = Encoding.UTF8.GetBytes(plainText);
+            return _dataProtector.Protect(bytes);
+        }
+
+        public string DecryptData(byte[] encryptData)
+        {
+            var bytes = _dataProtector.Unprotect(encryptData);
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
